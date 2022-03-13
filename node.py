7959f1f5c6b55fcf694acfd1ddd61b57ed782450
mlgepicbanana <MlgEpicBanana69@gmail.com>
2022-03-13 12:48:48 +0200

    Server

--- node.py
+++ node.py
@@ -7,3 +7,12 @@ from Crypto.Random import get_random_bytes
 from Crypto.Hash import SHA512
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Cipher import AES
+from my_server import Server
+
+
+VERSION = "1.0"
+
+server = Server("127.0.0.1")
+server.run()
+
+print("?")
