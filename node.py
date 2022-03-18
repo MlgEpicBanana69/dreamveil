01044729797fb956e0953d205e88b9752e25d707
mlgepicbanana <MlgEpicBanana69@gmail.com>
2022-03-18 20:12:43 +0200

    Removed redundent imports

--- node.py
+++ node.py
@@ -4,12 +4,6 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
-import secrets
-from getpass import getpass
-from Crypto.Random import get_random_bytes
-from Crypto.Hash import SHA512
-from Crypto.Protocol.KDF import PBKDF2
-from Crypto.Cipher import AES
 from my_server import Server
 import configparser
 
