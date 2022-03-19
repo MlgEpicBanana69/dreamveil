3445a7017de506670f76e4e3580e234abd3678ea
mlgepicbanana <MlgEpicBanana69@gmail.com>
2022-03-19 17:41:12 +0200

    Started implementing basic application UI

--- node.py
+++ node.py
@@ -1,8 +1,5 @@
 import dreamveil
 
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys
 
 from my_server import Server
 import configparser
@@ -16,13 +13,4 @@ server = Server(VERSION, application_config["SERVER"]["address"])
 p = server.connect("192.168.1.36")
 p.send("hello")
 
-def window():
-    node_app = QApplication(sys.argv)
-    node_window = QMainWindow()
-    node_window.setGeometry(650, 200, 1100, 650)
-    node_window.setWindowTitle("Dreamveil")
 
-    node_window.show()
-    sys.exit(node_app.exec_())
-
-window()
